f6ef61043b9a9a9058d9d65a6eb325a6e896c554
carlo-astro <carlo@ufrj.br>
2025-07-30 17:32:41 -0300

    🐍🐁 Floor and Shrub 25.07 (30)

--- _core/suucury.py
+++ _core/suucury.py
@@ -11,6 +11,10 @@ Changelog
    |br| Crude scenery creation (18).
    |br| key capture and paralax calculation (20).
    |br| port code to Suucuri github (26).
+.. versionadded::    25.07
+   |br| Added Monsters (28).
+   |br| Monster jinx (29).
+   |br| Path and front vegetation (30).
 
 |   **Open Source Notification:** This file is part of open source program **Suucurijuba**
 |   **Copyright © 2025  Carlo Oliveira** <carlo@nce.ufrj.br>,
@@ -21,12 +25,13 @@ from secrets import choice
 
 from vitollino import Jogo, Cena, Elemento
 from random import randint
-from random import randint
-FLORA = "https://i.imgur.com/n3GnL9B.png"
+
+FLORA = "../_media/flora.png"
+RAST = "../_media/rasteira.png"
 MATA0 = "https://i.imgur.com/TT2FKyu.jpeg"
 TORA = "https://imgur.com/0jSB27g.png"
 BALL = "https://i.imgur.com/rf8yRt0.png"
-FX, FY = 5, 4
+FX, FY = 5, 5
 KX, KY = 6, 1
 TREES = 300
 LAYERS = 8
@@ -37,10 +42,15 @@ IMPT = "../_media/imptikuna.png"
 TIMP = "../_media/ticunaimp.png"
 CURU = "../_media/curupira.png"
 MONS = [MAPU, IMPT, TIMP, MAPI, CURU, CURU]
+RANG = 200
+TOPO = 350
 
 
 class Lax:
     def __init__(self):
+        def calc_shrub(shrub):
+            shrub.w, shrub.h, shrub.y, = 80, 60, 350
+
         def calc_parallax(x, y, item):
             conta_, lado_ = x - 1 if x > 1 else 1, y - 1 if y > 1 else 1
             dw, dh = (100 / conta_) * (item % x), (100 / lado_) * (item // x)
@@ -53,14 +63,17 @@ class Lax:
         self.c = Cena(MATA0)
         self.c.elt.style.overflow = "hidden"
         self.c.vai()
-        self.layers = [Elemento(w=4000, h=700, cena=self.c) for _ in range(LAYERS)]  # [list()]*LAYERS
+        self.sprites = []
+        self.layers = [Elemento(w=4000, h=600, cena=self.c) for _ in range(LAYERS)]  # [list()]*LAYERS
+        [ly.elt.style.setProperty("pointer-events", "none") for ly in self.layers]  # [list()]*LAYERS
         self.scenery()
-        self.kiri = Elemento(KIRI, w=130, h=250, x=600, y=400, cena=self.c, vai=self.fire)
-        self.mapu = Elemento(MAPU, w=130, h=250, x=1020, y=400, cena=self.c)
+        self.kiri = Elemento(KIRI, w=130, h=250, x=600, y=TOPO, cena=self.c, vai=self.fire)
+        self.mapu = Elemento(MAPU, w=130, h=250, x=1020, y=TOPO, cena=self.c)
         self.mapu.img = TIMP
         self.mapu_h = None
-        self.monster(fig=choice(MONS), x=choice((200, 1000)))
-        self.ball = Elemento(BALL, w=4, h=4, x=680, y=500, cena=self.c, vai=self.fire)
+        self.range = [600 + RANG * rg for rg in (-1, 1)]
+        self.monster(fig=choice(MONS), x=choice(self.range))
+        self.ball = Elemento(BALL, w=4, h=4, x=680, y=TOPO + 100, cena=self.c, vai=self.fire)
         self.ball.elt.bind("transitionend", self.hexed)
         # self.ball.elt.style.display = "none"
         self.sp = sp = 0.5
@@ -68,9 +81,24 @@ class Lax:
         self.kiri_go = 1
         self.tr = f"width {sp}s, height {sp}s, left {sp}s, top {sp}s, opacity {sp}s, transform {sp}s, easy-in 0.5s"
         self.ball.elt.style.transition = self.tr
-        kr = self.sprite_kiri(self.spriter, Elemento(KIRI, w=25, h=50, x=1200, y=400, cena=self.c, vai=self.right))
-        kl = self.sprite_kiri(self.spritel, Elemento(KIRI, w=25, h=50, x=50, y=400, cena=self.c, vai=self.left))
         self.k = self.sprite_kiri(self.spriter, self.kiri)
+        grass = self.layers[-1]
+        solo = self.layers[-2]
+        solo.img = "../_media/solo.jpg"
+        solo.h = 50
+        solo.y = 550
+        solo.elt.style.backgroundRepeat = "repeat-x"
+        solo.elt.style.backgroundSize = "100px"
+        solo.elt.style.filter = "blur(0.03rem)"
+        grass.entra(self.c)
+        grass.y = 50
+        grass.h = 450
+        grass.x = 1000
+        grass.w = 4000
+        _ = [calc_shrub(shrub) for shrub in self.sprites[-1]]
+        kr = self.sprite_kiri(self.spriter,
+                              Elemento(KIRI, w=25, h=50, x=1200, y=TOPO - 150, cena=self.c, vai=self.right))
+        kl = self.sprite_kiri(self.spritel, Elemento(KIRI, w=25, h=50, x=50, y=TOPO - 150, cena=self.c, vai=self.left))
 
     def monster(self, fig=None, x=None, o=0.0, calm=True):
         self.is_calm = calm
@@ -96,9 +124,9 @@ class Lax:
         self.mapu_h += 1
         self.ball.o = 0
         self.ball.x = 680
-        self.ball.y = 500
+        self.ball.y = TOPO + 100
         self.ball.w = self.ball.h = 4
-        self.monster(fig=choice(MONS), x=choice((200, 1000)), o=0.0) if self.mapu_h > 3 else None
+        self.monster(fig=choice(MONS), x=choice(self.range), o=0.0) if self.mapu_h > 3 else None
 
     def fire(self, evento):
         evento.stopPropagation()
@@ -109,9 +137,9 @@ class Lax:
         self.ball.elt.style.transform = "rotate(720deg)"
         self.ball.o = 1.0
         self.ball.w = self.ball.h = 180
-        self.ball.x = 600 + self.kiri_go * 400
+        self.ball.x = 600 + self.kiri_go * RANG
         # self.ball.x = 1000
-        self.ball.y = 400
+        self.ball.y = TOPO
 
     def left(self, evento):
         evento.stopPropagation()
@@ -132,6 +160,7 @@ class Lax:
     def move(self, val=40):
         def mover(lay, val_):
             lay.x = lay.x + val_
+
         print(self.layers[0].x)
         self.kiri_x += 1
         if self.kiri_x > 3:
@@ -146,8 +175,12 @@ class Lax:
             layer.elt.style.scale = scale
             layer.elt.style.transition = "left 1s"
 
-        _ = [lay.elt <= self.sprite(150 * item - 3000, 350, randint(0, 15), 1, layer)
-         for layer, lay in enumerate(self.layers) for item in range(0, trees)]
+        last = len(self.layers) - 2
+        self.sprites = [[self.sprite(
+            150 * item - 3000, 350, randint(0, 15), 1, layer, FLORA if layer < last else RAST)
+            for item in range(0, trees if layer < last else trees * 3)]
+            for layer, lay in enumerate(self.layers)]
+        _ = [lay.elt <= sprite.elt for lay, sprites in zip(self.layers, self.sprites) for sprite in sprites]
         [off_lay(lay, layer) for layer, lay in enumerate(self.layers)]
 
     def sprite_kiri(self, item, e):
@@ -158,20 +191,20 @@ class Lax:
         e.elt.style.backgroundPosition = bp
         return e.elt
 
-    def sprite(self, x, y, item, layer, ly, elt=None):
+    def sprite(self, x, y, item, layer, ly, sheet=FLORA, elt=None):
         """Near layer should be more spaced"""
-        item = randint(0, 14)
+        item = randint(0, 25)
         layer_delta_y = 400 // LAYERS
         conta_, lado_, dw, dh = self.calc(FX, FY, item)
         bp = f"{dw:.2f}% {dh:.2f}%"
         size = TREES - layer * 30
-        e = elt or Elemento(FLORA, w=size - 10, h=size, x=x, y=y - layer * layer_delta_y, cena=self.c)
+        dx = 1 if sheet == FLORA else 3
+        e = elt or Elemento(sheet, w=size - 10, h=size, x=x//dx, y=y - layer * layer_delta_y, cena=self.c)
         e.elt.style.backgroundSize = f"{FX * 100}% {FY * 100}%"
         e.elt.style.backgroundPosition = bp
-        return e.elt
+        return e
 
 
 def main():
     Jogo(style=dict(height="650px", width="1300px"), did="app").z()
     Lax()
-
